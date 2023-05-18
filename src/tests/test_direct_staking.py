d9d2b8fc0a29afa501b26b2667d1e0349bc7fd63
xtaci <daniel820313@gmail.com>
2023-05-18 13:50:09 +0800

    add case2 for forceExit

--- src/tests/test_direct_staking.py
+++ src/tests/test_direct_staking.py
@@ -41,6 +41,36 @@ def test_forceExit(setup_contracts, owner, pubkeys, sigs, signerPrivate, withdra
     with brownie.reverts("EXITING"):
         transparent_ds.forceExit(0, True, {'from':owner})
 
+    with brownie.reverts("EXITING"):
+        transparent_ds.forceExit(0, False, {'from':owner})
+
+""" test of force exit a validator without mev rewards claiming"""
+def test_forceExit2(setup_contracts, owner, pubkeys, sigs, signerPrivate, withdraw_address):
+    transparent_ds, transparent_rewardpool = setup_contracts
+    claimAddr = owner.address
+
+    ''' sign digest in EIP-191 standard '''
+    md = digest(0, transparent_ds.address, claimAddr, withdraw_address, pubkeys, sigs)
+    message = encode_defunct(md.digest())
+    signed_message = Account.sign_message(message, private_key=signerPrivate)
+    transparent_ds.stake(claimAddr, withdraw_address, pubkeys, sigs, bytes(signed_message.signature), 0, 0, {"from":owner, 'value': '64 ether'})
+
+    ''' Transfer 0.1 eth as MEV revenue '''
+    owner.transfer(transparent_rewardpool.address, '0.1 ethers')
+
+    ''' Initiating force exit '''
+    balanceBeforeExit = owner.balance()
+    mevRewards = transparent_rewardpool.getPendingReward(claimAddr)
+    transparent_ds.forceExit(0, False, {'from':owner})
+
+    assert owner.balance() == balanceBeforeExit
+    assert transparent_rewardpool.getPendingReward(claimAddr) == mevRewards
+   
+    ''' forceExit again should revert '''
+    with brownie.reverts("EXITING"):
+        transparent_ds.forceExit(0, False, {'from':owner})
+
+
 def digest(extraData, contractAddr, claimaddr, withdrawaddr, pubkeys, signatures):
     #print(EthAddress(claimaddr))
     abi = eth_abi.encode(['uint256','address', 'uint256', 'address', 'address'], [extraData, contractAddr, Chain().id, claimaddr, convert.to_address(withdrawaddr)])
