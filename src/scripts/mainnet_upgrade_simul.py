d215f20b8402d5e72218e45596e5843ca7f40307
xtaci <daniel820313@gmail.com>
2024-02-07 21:45:24 +0800

    add simul

--- src/scripts/mainnet_upgrade_simul.py
+++ src/scripts/mainnet_upgrade_simul.py
@@ -1,6 +1,7 @@
 from brownie import *
 from brownie import convert
 from brownie.convert import EthAddress
+from brownie.network.state import Chain
 from eth_account.messages import encode_defunct
 from eth_account import Account
 from pathlib import Path
@@ -18,6 +19,7 @@ def main():
     gnosis_safe = accounts.at('0xAeE017052DF6Ac002647229D58B786E380B9721A', {'force':True})
     proxy_admin_contract = ProxyAdmin.at('0xa5F2B6AB5B38b88Ba221741b3A189999b4c889C6')
     direct_staking_proxy = '0xe8239B17034c372CDF8A5F8d3cCb7Cf1795c4572'
+    user = accounts[0]
     deployer = accounts.load('mainnet-deployer')
 
     ### deploy staking contract
@@ -31,3 +33,49 @@ def main():
     print(transparent_direct_staking.DEPOSIT_SIZE())
     print(transparent_direct_staking.getExitQueueLength())
     #print(rewardpool_contract.CONTROLLER_ROLE())
+
+    # signer privkey
+    signerPub = "0x2C4594B11BaAD822B5be6a65348779Bb97473682"
+    signerPrivate = "a441e60dd489bdfa4a848bee22d9225a6d53f4aadad492ccae5014e1d88d84cc"
+
+    transparent_direct_staking.setSigner(signerPub, {'from': gnosis_safe})
+
+    #stake
+    # staker info prepare
+    print("Preparing signature for staking....")
+    pubkey = 0x99380e442ac9955cd0b82a820f4d2b5a630cc0b24fa57f1d0f80dd42fcc1be92ac4038b29de057e9b62c7783103651f9
+    pubkey2 = 0xae73a54c8206f664523e4a45f802c6b3b8f7bdb9a8c64f2af53bf7c4425e350c68cd906ec822e1ec84e8e3d626f958f3
+    claimAddr = user.address
+    withdrawAddr = "0x11ad6f6224eaad9a75f5985dd5cbe5c28187e1b7"
+    signature = 0xa2f1845644cee06469cea42dbd5ebf4505b9489ed896788ab2b8e42124aceb88a6565a375546254f5507b425d15c90a10e772708dbe9a56b3e46f5c47e8aaf6a9849ae4f838bb9bac068bcde47b616fd2b0824de23ec17981987668a4c50e17d
+    signature2 = 0xb337f858d1938704cdb2e5bf5dfb82723f7f5a08b6ce66200d24efa3973132dd3e701111cccf940c5965e80b5068af830be5e9d1ca1aa06e57ddd7b3948501f16e79c48e039738836ca4e5f3442b5e5c52eff472b4526a973649d0dad73698d5
+    md = digest(0, transparent_direct_staking.address, claimAddr, withdrawAddr, [pubkey, pubkey2], [signature, signature2])
+
+    print("Digest:", md.hexdigest())
+
+    # sign digest in EIP-191 standard
+    message = encode_defunct(md.digest())
+    print("Message:", message)
+    signed_message = Account.sign_message(message, private_key=signerPrivate)
+    print("Signature:", signed_message)
+
+    print("Initiate 64 ETH Staking")
+    # ecrecover in Solidity expects the signature to be split into v as a uint8,
+    #   and r, s as a bytes32
+    # Remix / web3.js expect r and s to be encoded to hex
+    print(signed_message.signature, bytes(signed_message.signature))
+    transparent_direct_staking.stake(claimAddr, withdrawAddr, [pubkey, pubkey2], [signature,signature2], bytes(signed_message.signature), 0,'0.1 ether',{"from":user, 'value': '64.1 ether'})
+
+def digest(extraData, contractAddr, claimaddr, withdrawaddr, pubkeys, signatures):
+    #print(EthAddress(claimaddr))
+    abi = eth_abi.encode_abi(['uint256','address', 'uint256', 'address', 'address'], [extraData, contractAddr, Chain().id, claimaddr, convert.to_address(withdrawaddr)])
+    digest = hashlib.sha256(abi)
+
+    for i in range(len(pubkeys)):
+        pubkey = pubkeys[i]
+        signature = signatures[i]
+        abi = eth_abi.encode_abi(['bytes32', 'bytes', 'bytes'], [convert.to_bytes(digest.hexdigest(),"bytes32"), convert.to_bytes(pubkey,"bytes"), convert.to_bytes(signature,"bytes")])
+        digest = hashlib.sha256(abi)
+
+    return digest
+
