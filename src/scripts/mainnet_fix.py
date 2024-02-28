125a208f09c249eb6362a3b54331061ae1d674d9
xtaci <daniel820313@gmail.com>
2024-02-28 19:20:02 +0800

    upd py

--- src/scripts/mainnet_fix.py
+++ src/scripts/mainnet_fix.py
@@ -17,5 +17,6 @@ def main():
     me = accounts.at('0x75fE76d459e8ca4440822f7D90aa56a222726EB6', {'force':True})
     accounts[0].transfer(me, '10 ether')
     for x in range(130):
-        me.transfer(me, 1)
-        # TransparentUpgradeableProxy.deploy('0x62cea417a02cac2433df622fe3a0eb0662f1ca61', me, b'', {'from':me})
+        me.transfer(me, 0)
+
+    #TransparentUpgradeableProxy.deploy('0x62cea417a02cac2433df622fe3a0eb0662f1ca61', me, b'', {'from':me})
